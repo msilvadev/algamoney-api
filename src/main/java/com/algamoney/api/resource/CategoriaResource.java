cc2de05011cfb9f8e9da7f7b107a275395b958b0
Matheus Silva <msilva.jus@gmail.com>
2018-10-10 22:04:55 -0300

    Delete Category

--- src/main/java/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -9,11 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -52,4 +55,16 @@ public class CategoriaResource {
 		Categoria categoria = categoriaRepository.findById(codigo).orElse(null);
 		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();	
 	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletarPeloCodigo(@PathVariable Long codigo){
+		categoriaRepository.deleteById(codigo);
+	}	
 }
+
+
+
+
+
+
