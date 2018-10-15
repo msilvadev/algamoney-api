0e574bd412cb680ead404db93033563356cef4b4
Matheus Silva <msilva.jus@gmail.com>
2018-10-14 21:52:05 -0300

    inicio cadastro pessoas

---
+++ src/main/java/com/algamoney/api/resource/PessoaResouce.java
@@ -0,0 +1,42 @@
+package com.algamoney.api.resource;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.algamoney.api.model.Pessoa;
+import com.algamoney.api.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResouce {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@GetMapping
+	public List<Pessoa> listarPessoas(){
+		return pessoaRepository.findAll();
+	}
+	
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
+		
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	}
+}
